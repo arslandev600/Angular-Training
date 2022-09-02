bbd0947fc92f9432aafb945753bd75e76b4a8e77
Arslan Haider <arslan.dev600@gmauil.com>
2022-09-02 23:53:08 +0500

    Angular Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './Home/about/about.component';
+import { ContactComponent } from './Home/contact/contact.component';
+import { HomeComponent } from './Home/home/home.componen';
+import { SignUpComponent } from './Home/signup/signup.component';
+import { NavComponent } from './navbar/nav.component';
+import { NotFoundComponent } from './pagenotfound/not-found.component';
 
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'about',component:AboutComponent},
+  {path:'contact',component:ContactComponent},
+  {path:'signup',component:SignUpComponent},
+  {path:'**',component:NotFoundComponent},
+  
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    ContactComponent,
+    SignUpComponent,
+    AboutComponent,
+    FooterComponent,
+    NotFoundComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pagenotfound/not-found.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-not-found',
+    template: '<h1>PAge your are looking is not Available</h1>'
+})
+
+export class NotFoundComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
